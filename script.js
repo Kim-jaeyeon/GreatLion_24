f136fde7fbce5d68a8ab404a5a24e1001c921901
Kim-jaeyeon <jy020507@naver.com>
2024-03-20 00:22:16 +0900

    새로운 페이지 제작
    사진을 누르면 자기소개 페이지로 넘어가서 여러 항목들을 보여줌.

---
+++ script.js
@@ -0,0 +1,13 @@
+var clock=document.getElementById("clock");
+
+function getTime(){
+    var timeInfo=new Date();
+    var hour=String(timeInfo.getHours()).padStart(2,'0');
+    var min=String(timeInfo.getMinutes()).padStart(2,'0');
+    var sec=String(timeInfo.getSeconds()).padStart(2,'0');
+
+    clock.innerText=hour+":"+min+":"+sec;
+    console.log("!");
+}
+getTime();
+setInterval(getTime,1000);
